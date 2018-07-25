005c4e1ca5bdcde410213ca2030c18491c998ede
Joseph Branham Ricohermoso <branhamricohermoso@gmail.com>
2018-07-26 01:56:21 +0800

    initial commit for this demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { preserveWhitespacesDefault, splitClasses } from '@angular/compiler';
+import { stringify } from '@angular/core/src/util';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,101 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  public items = [
+    [
+      { name: 'fries', title: 'Fries', quantity: 10, price: 1.25 },
+      { name: 'burger_single', title: 'Single Patty Burger', quantity: 10, price: 2 },
+      { name: 'burger_double', title: 'Double Patty Burger', quantity: 10, price: 2.25 },
+    ],
+    [
+      { name: 'sundae', title: 'Sundae', quantity: 10, price: .75 },
+      { name: 'choco_fudge', title: 'Choco Fudge', quantity: 10, price: 1.25 },
+      { name: 'strawberry_fudge', title: 'Strawberry Fudge', quantity: 10, price: 1.25 },
+    ]
+  ];
+
+  public cartItems = [];
+  public cart = [];
+
+  public orderSuccess = false;
+  public submitting = false;
+  public submittedResponse;
+
+  private websocket;
+
+  constructor() {
+
+    let factory;
+    try {
+      factory = new WebSocketFactory();
+      this.websocket = factory.createWebSocket('ws://demos.kaazing.com/echo');
+
+      this.websocket.onopen = (evt) => {
+        console.log('CONNECTED');
+      };
+
+      this.websocket.onmessage = (evt) => {
+        const data = evt.data;
+        if (typeof(data) === 'string') {
+          this.submitting = false;
+          this.orderSuccess = true;
+          this.submittedResponse = data;
+
+          setTimeout(() => this.orderSuccess = false, 5000);
+        }
+      };
+
+      this.websocket.onclose = (evt) => {
+        console.log('CLOSED: (' + evt.code + ') ' + evt.reason);
+      };
+    } catch (e) {
+
+    }
+  }
+
+  groupBy(collection, property) {
+    let i = 0, val, index, values = [], result = [];
+    for (; i < collection.length; i++) {
+        val = collection[i][property];
+        index = values.indexOf(val);
+        if (index > -1) {
+          result[index].push(collection[i]);
+        } else {
+          values.push(val);
+          result.push([collection[i]]);
+        }
+    }
+    return result;
+}
+
+  add(rowIndex, itemIndex) {
+    const item = this.items[rowIndex][itemIndex];
+    this.cartItems.push(item);
+
+    this.cart = this.groupBy(this.cartItems, 'name');
+  }
+
+  removeItem(cartIndex) {
+    const item = this.cart[cartIndex].pop();
+    if (this.cart[cartIndex].length === 0) {
+      this.cart.splice(cartIndex, 1);
+    }
+    const i = this.cartItems.findIndex(cI => cI.name === item.name);
+    this.cartItems.splice(i, 1);
+  }
+
+  total() {
+    return this.cartItems.reduce((p, n) => (p + n.price), 0);
+  }
+
+  submit() {
+    this.submitting = true;
+    this.orderSuccess = false;
+    this.submittedResponse = '';
+    this.websocket.send(JSON.stringify({ 'event': 'purchase', 'amount': this.total() }));
+
+    this.cart = [];
+    this.cartItems = [];
+  }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,5 +1,6 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+declare var WebSocketFactory: any;
 interface NodeModule {
   id: string;
 }
